bf603177b9e72d8a430c69cd4dd73b701b4a0747
AdiyamGN <adiyamgn@outlook.com>
2016-04-11 19:41:38 -0500

    added an onSubmit function in the controller.js and added the conditionals on the email input to show error messages.

--- js/form_validation_controller.js
+++ js/form_validation_controller.js
@@ -11,6 +11,17 @@
 		function FormValidationController() {
 			var vm = this;
 			vm.formModel = {};
+
+			vm.onSubmit = function(valid) {
+				if(valid) {
+					console.log('Hey I am submitted!');
+					console.log(vm.formModel);
+				} else {
+					console.log('The Form is invalid');
+				}
+				
+
+			}
 		}
 
 
